5f953391cc4e99defc66272fbc38c94132c5f612
KacperKoow <kacper.kowalski799@gmail.com>
2025-08-05 23:13:18 +0200

    Update projects component

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -4,7 +4,7 @@ export const PROJECTS = [
     description:
       'This is a personal portfolio web application built using Angular, TypeScript, HTML, and CSS. It presents my technical skills, selected projects, and educational background. The site also includes a contact form that allows visitors to send me messages directly, providing a simple way for potential collaborators or employers to reach out.',
     image: 'portfolio.png',
-    websitePath: '',
+    websitePath: 'https://portfolio-5f7b3.firebaseapp.com/',
     githubPath: 'https://github.com/KacperKoow/Portfolio',
   },
 ];
