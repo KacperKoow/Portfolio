8a5423c31a94dcaaf1c17372ebec61270f1c2d98
KacperKoow <kacper.kowalski799@gmail.com>
2025-08-05 21:35:37 +0200

    Update nav component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { NavService } from './nav.service';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
-export class NavComponent {}
+export class NavComponent {
+  private navService = inject(NavService);
+
+  onNavigateToSection(section: string) {
+    this.navService.navigateToSection(section);
+  }
+}
