f84f0e097b991a02d7f8ce9aa38d9a41debae1a7
KacperKoow <kacper.kowalski799@gmail.com>
2025-08-05 21:33:37 +0200

    Update contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faLinkedin = faLinkedin;
+  faPaperPlane = faPaperPlane;
 
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      alert('Please enter valid information in all required fields.');
+      return;
+    }
+    emailjs.init('PU3i1gbVeoMgsd_Iv');
+    emailjs.send('service_ppon2po', 'template_nq7dvwf', {
+      name: formData.value.name,
+      email: formData.value.email,
+      message: formData.value.message,
+    });
+    alert('Your message has been sent successfully.');
+    formData.reset();
+  }
 }
