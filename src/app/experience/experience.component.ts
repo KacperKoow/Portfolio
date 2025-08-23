78c460ff3e2518a2930c454f13426e614fb2a567
KacperKoow <kacper.kowalski799@gmail.com>
2025-08-23 09:56:46 +0200

    Implement responsive experience section

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,12 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrl: './experience.component.css',
 })
-export class ExperienceComponent {
-
-}
+export class ExperienceComponent {}
